b0c5d5466f1b7a5aedbc5daf5e363eb16da92fae
Xindi LI <xil573@eng.ucsd.edu>
2017-02-24 15:36:32 -0800

    update

---
+++ public/js/AddNewAccount.js
@@ -0,0 +1,12 @@
+﻿function backToAddContactPage() {
+    var url = window.location.href;
+    var index = url.indexOf("ount/");
+    var idStr = url.substring(index + 5);
+    window.location.href = "/?id=" + idStr;
+}
+function searchResult() {
+    var $buttonClicked = document.getElementById('search_str');
+    var name = $buttonClicked.value;
+    console.log($buttonClicked.value);
+    window.location.href = "../search-result?name=" + name;
+}
---
+++ public/js/EditProfile.js
@@ -0,0 +1,36 @@
+﻿function add() {
+    var success = 0;
+    var url = window.location.href;
+    var index = url.indexOf("/edit-profile/");
+    var idStr = url.substring(index + 14);
+    var title = document.getElementById('title');
+    if (title.value.length == 0) {
+        alert("Title cannot be empty!")
+    } else {
+        var sub = document.getElementById('sub');
+        if (sub.value.length == 0) {
+            alert("Subtitle cannot be empty!")
+        } else {
+            var con = document.getElementById('con');
+            if (con.value.length == 0) {
+                alert("Content cannot be empty!")
+            } else {
+                $.get('/edit-profile/' + idStr + '/' + title.value + '/' + sub.value + '/' + con.value, review);
+            }
+        }
+    }    
+}
+
+function review(result) {
+    if (result == 1) {
+        alert("Add successful!")
+    }
+}
+
+function backToIndex() {
+    var url = window.location.href;
+    var index = url.indexOf("/edit-profile/");
+    var idStr = url.substring(index + 14);
+    console.log(idStr);
+    window.location.href = "../my-profile/" + idStr;
+}
